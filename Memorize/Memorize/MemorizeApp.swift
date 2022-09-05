d4a72b09544867ea304165d612c415f3dbbd3d47
Elaidzha1940 <eli080293@gmail.com>
2022-09-05 21:36:27 +0300

    5.09

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Elaidzha Shchukin on 05.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
